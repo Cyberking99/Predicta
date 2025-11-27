79d4dcb9395364df3e1fad849e59b27bff0b7108
Kefas Kingsley <kefasiceking@gmail.com>
2025-11-27 20:56:15 +0100

    chore: asset optimization

--- frontend/dev-dist/assets/decimals-CXPoFdP8.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-CzYOAarU.js";import{r as a}from"./index-e-lRfTYT.js";const r="0x313ce567",e=[],n=[{type:"uint8"}];async function o(c){return a({contract:c.contract,method:[r,e,n],params:[]})}async function i(c){return t(()=>o(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
--- frontend/dev-dist/assets/eth_estimateGas-EEB9H-qN.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./index-CzYOAarU.js";async function m(t,e){const a=await t({method:"eth_estimateGas",params:[e]});return s(a)}export{m as eth_estimateGas};
